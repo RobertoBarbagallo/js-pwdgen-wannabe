db76a6bc16692e55662c96f5d3730297c2987df5
RobertoBarbagallo <roberto-barbagallo@live.it>
2021-04-07 14:52:02 +0200

    eseercizio comletato

---
+++ myPlugIn.js
@@ -0,0 +1,6 @@
+var ilNome = prompt ("Inserisci qui il tuo nome");
+var ilCognome = prompt ("Inserisci qui il tuo cognome");
+var ilColore = prompt ("Inserisci qui il tuo colore preferito");
+
+document.getElementById("target").innerHTML = "La tua Password è: " + ilNome + ilCognome + ilColore + "21";
+;
